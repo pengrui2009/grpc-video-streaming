3a16e1ff2384ae00643cd9199a6b55f26f55a184
szymek325 <matszym325@gmail.com>
2020-02-16 20:56:28 +0100

    Cleanup client script

--- src/client.py
+++ src/client.py
@@ -4,6 +4,11 @@ import shared.video_frame_pb2_grpc as video_frame_pb2_grpc
 import numpy as np
 import cv2
 from PIL import Image
+import datetime as dt
+
+SERVER_ADDRESS = '127.0.0.1:50051'
+CAMERA_SOURCE = 0
+WIDTH, HEIGHT = 280, 280
 
 
 def infinity_loop(cap):
@@ -14,25 +19,27 @@ def infinity_loop(cap):
 
 def generateRequests():
     print('Accessing camera stream')
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(CAMERA_SOURCE)
     for _ in infinity_loop(cap):
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        width_d, height_d = 280, 280  # Declare your own width and height
-        face = cv2.resize(gray, (width_d, height_d))
+        face = cv2.resize(gray, (WIDTH, HEIGHT))
         pil_image = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
         pil_image = Image.fromarray(pil_image).convert('L')
         numpy_array = np.array(pil_image, 'uint8')
-        print(numpy_array)
-        print(type(numpy_array))
-        yield video_frame_pb2.FrameRequest(img=numpy_array.tobytes())
+        yield video_frame_pb2.FrameRequest(
+            img=numpy_array.tobytes(),
+            width=WIDTH,
+            height=HEIGHT,
+            fps=30,
+            isColor=False)
 
 
 def run():
-    channel = grpc.insecure_channel('127.0.0.1:50051')
+    channel = grpc.insecure_channel(SERVER_ADDRESS)
     stub = video_frame_pb2_grpc.VideoFrameStub(channel)
     for response in stub.Send(generateRequests()):
-        print(str(response.reply))
+        print(f"{dt.datetime.now()}: {str(response.reply)}")
 
 
 if __name__ == '__main__':
