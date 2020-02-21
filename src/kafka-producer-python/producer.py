bc9afefc1fc883c2cc0813080cf37710b707f9ec
szymek325 <matszym325@gmail.com>
2020-02-21 13:54:05 +0100

    Add python kafka producer

---
+++ src/kafka-producer-python/producer.py
@@ -0,0 +1,25 @@
+from confluent_kafka import Producer
+
+
+p = Producer({'bootstrap.servers': 'localhost:9092'})
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+while True:
+    # Trigger any available delivery report callbacks from previous produce() calls
+    p.poll(0)
+
+    # Asynchronously produce a message, the delivery report callback
+    # will be triggered from poll() above, or flush() below, when the message has
+    # been successfully delivered or failed permanently.
+    p.produce('test-topic', "test123", callback=delivery_report)
+
+# Wait for any outstanding messages to be delivered and delivery report
+# callbacks to be triggered.
+p.flush()
